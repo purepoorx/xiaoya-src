72a2184d24de9e78a67c0652983846d5e3f5c382
purepoorx <purepoorx@users.noreply.github.com>
2024-10-17 20:48:22 +0800

    Apply automatic changes

--- data/update_data.sh
+++ data/update_data.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 base_urls=(
-    "https://raw.githubusercontent.com/xiaoyaliu00/data/main"
+    "https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://cdn.wygg.shop/https://raw.githubusercontent.com/xiaoyaliu00/data/main"
-    "https://fastly.jsdelivr.net/gh/xiaoyaliu00/data@latest"
-    "https://521github.com/extdomains/github.com/xiaoyaliu00/data/raw/main"
-    "https://cors.zme.ink/https://raw.githubusercontent.com/xiaoyaliu00/data/main"
+    "https://521github.com/extdomains/github.com/xiaoyaliu00/data/raw/main/"
+    "https://cors.zme.ink/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://git.jasonml.xyz/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://cdn.wygg.shop/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://gh.ddlc.top/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://git.886.be/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://gh.idayer.com/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
     "https://slink.ltd/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://raw.yzuu.cf/xiaoyaliu00/data/main/"
+    "https://raw.nuaa.cf/xiaoyaliu00/data/main/"
+    "https://raw.kkgithub.com/xiaoyaliu00/data/main/"
+    "https://ghp.ci/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://gitdl.cn/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://gh.con.sh/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://ghproxy.net/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://github.moeyy.xyz/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://gh-proxy.com/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://ghproxy.cc/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://gh.llkk.cc/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
+    "https://gh-proxy.llyke.com/https://raw.githubusercontent.com/xiaoyaliu00/data/main/"
 )
 
 success=false
 for base_url in "${base_urls[@]}"; do
-    remote_ver=$(curl --ipv4 ${base_url}/version.txt 2>/dev/null)
+    #remote_ver=$(curl --ipv4 ${base_url}/version.txt 2>/dev/null)
+    remote_ver=$(curl --ipv4 ${base_url}/version.txt 2>/dev/null | grep '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
     if [ $? -eq 0 ]; then
         success=true
 		download_url="${base_url}"
