b945cbad806698a87bc4840d98be14a0e560f192
purepoorx <107200810+purepoorx@users.noreply.github.com>
2025-03-06 04:27:26 +0800

    Apply automatic changes

--- data/update_data.sh
+++ data/update_data.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 base_urls=(
-    "https://slink.ltd/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
-    "https://raw.githubusercontent.com/xiaoyaDev/data/main/"
-    "https://gitdl.cn/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
-    "https://gh-proxy.com/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
-    "https://ghproxy.cc/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
-    "https://gh.llkk.cc/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
+    "https://slink.ltd/https://raw.githubusercontent.com/xiaoyaDev/data/main"
+    "https://raw.githubusercontent.com/xiaoyaDev/data/main"
+    "https://gitdl.cn/https://raw.githubusercontent.com/xiaoyaDev/data/main"
+    "https://gh-proxy.com/https://raw.githubusercontent.com/xiaoyaDev/data/main"
+    "https://ghproxy.cc/https://raw.githubusercontent.com/xiaoyaDev/data/main"
+    "https://gh.llkk.cc/https://raw.githubusercontent.com/xiaoyaDev/data/main"
 )
-#"https://github.moeyy.xyz/https://raw.githubusercontent.com/xiaoyaDev/data/main/"
 
 #if [ -f /data/download_url.txt ]; then
 #	download_url=$(head -n1 /data/download_url.txt)
 #else
 	success=false
 	for base_url in "${base_urls[@]}"; do
-    	remote_ver=$(curl --ipv4 ${base_url}/version.txt 2>/dev/null | grep '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
+    	remote_ver=$(curl --ipv4 ${base_url}/version.txt 2>/dev/null | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
     	if [ $? -eq 0 ]; then
         	success=true
 			download_url="${base_url}"
@@ -34,7 +33,7 @@ data_dir="/www/data"
 mkdir -p "${data_dir}"
 touch "${data_dir}/version.txt"
 local_ver=$(cat "${data_dir}/version.txt")
-if [ "$(printf '%s\n' "$local_ver" "$remote_ver" | sort -V | head -n1)" == "$local_ver" ] || [ ! -f "${data_dir}/tvbox.zip" ] || [ ! -f "${data_dir}/update.zip" ] || [ ! -f "${data_dir}/index.zip" ]; then
+if [ "$(printf '%s\n' "$local_ver" "$remote_ver" | sort -V | head -n1)" != "$remote_ver" ] || [ ! -f "${data_dir}/tvbox.zip" ] || [ ! -f "${data_dir}/update.zip" ] || [ ! -f "${data_dir}/index.zip" ]; then
 echo "最新版本 $remote_ver 开始更新下载....."
     echo ""	
     if curl --ipv4 --insecure -fsSL -o "${data_dir}/tvbox.zip" $download_url/tvbox.zip; then echo "成功更新 tvbox.zip"; fi 
