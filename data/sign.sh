5849574354b49575b6d165192173530a407ff8a0
purepoorx <107200810+purepoorx@users.noreply.github.com>
2025-06-05 21:39:18 +0800

    Apply automatic changes

--- data/sign.sh
+++ data/sign.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
-direct_link=$(cat /opt/alist/alist | grep -ao "\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}" | sed "s/\\\$/\$dollar/g")
-sign_cond=$(cat /opt/alist/alist | grep -ao '[a-z]*!=="preview"&&[a-z]*\.sign' | sed "s/\\\$/\$dollar/g")
-sign_str=$(cat /opt/alist/alist | grep -ao '\?sign=\$\{[a-z]*\.sign\}' | sed "s/\\\$/\$dollar/g")
-direct_link_sign=$direct_link
-if [ ! -f /data/nosign.txt ] && [ -f /data/guestpass.txt ] && [ -f /data/guestlogin.txt ]; then
-    sign=$(cat /data/guestpass.txt | tr -d '\r\n' | md5sum | awk '{print $1}')
-    js='
-(function(){
-    return "?sign='"$sign"'";
-})();
-'
-    js=$(echo -n "$js" | base64 -w 0)
-    direct_link_sign="$(cat /opt/alist/alist | grep -ao "\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}" | sed "s/\\\$/\$dollar/g")\$dollar{(()=>{const encodedFunc = \"$js\"; const decodedFunc = atob(encodedFunc); return eval(decodedFunc);})()}"
-fi
-
 # 配置文件路径
 CONFIG_FILE="/etc/nginx/http.d/default.conf" 
 
-alist_address="http://127.0.0.1:$(cat $CONFIG_FILE | grep -o ':[0-9]*/d/' | tr -d ':' | awk -F'/' '{print $1}'| head -n1)"
+direct_link=$(cat /opt/alist/alist | grep -ao "\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}\\\$\\{[a-z]*\\}" | sed "s/\\\$/\$dollar/g")
+direct_link_sign=$direct_link
+
+sign_cond=$(cat /opt/alist/alist | grep -ao '[a-z]*!=="preview"&&[a-z]*\.sign' | sed "s/\\\$/\$dollar/g")
+sign_str=$(cat /opt/alist/alist | grep -ao '\?sign=\$\{[a-z]*\.sign\}' | sed "s/\\\$/\$dollar/g")
+
+#这里有两个匹配的，第二处不知道什么应用场景，仅修改第一处。
+login_cmd=$(cat /opt/alist/alist | grep -ao '[a-zA-Z]\.success.*?login\.success.*?,.*?.token\),' | head -n1 | sed "s/\\\$/\$dollar/g")
+login_cmd_sign=$login_cmd
+post_api=$(cat /opt/alist/alist | grep -ao '[a-zA-Z]*\.post\("/auth/login"' | awk -F'(' '{print $1}' | head -n1)
+
+location_getsignmd5=$(cat "$CONFIG_FILE" | grep "cgi-bin/soutv" | sed 's/^\s*//g' | head -n1)
+
+if [ ! -f /data/nosign.txt ] && [ -f /data/guestpass.txt ] && [ -f /data/guestlogin.txt ]; then
+    sign=$(cat /data/guestpass.txt | tr -d '\r\n' | md5sum | awk '{print $1}')
+    echo -n "$sign" >/index/md5
+
+    #给URL加上签名
+    js='
+(function(){
+    return "?sign=" + localStorage.getItem("signmd5");
+})();
+'
+    js=$(echo -n "$js" | base64 -w 0)
+    direct_link_sign="$direct_link\$dollar{(()=>{const encodedFunc = \"$js\"; const decodedFunc = atob(encodedFunc); return eval(decodedFunc);})()}"
+
+    #登录的同时远程获取md5
+    js='
+(function(){
+    '"$post_api"'("/getsignmd5", "cat md5", {
+  headers: {
+    "Content-Type": "text/plain"
+  }
+})
+        .then(response => {
+            localStorage.setItem("signmd5", response);
+        });
+})();
+'
+    js=$(echo -n "$js" | base64 -w 0)
+    login_cmd_sign="$login_cmd(()=>{const encodedFunc = \"$js\"; const decodedFunc = atob(encodedFunc); eval(decodedFunc);})(),"
+
+fi
+
+alist_address="http://127.0.0.1:$(cat "$CONFIG_FILE" | grep -o ':[0-9]*/d/' | tr -d ':' | awk -F'/' '{print $1}'| head -n1)"
 
 sed -i "s/XIAOYASIGN/$sign/g" /etc/nginx/http.d/emby.js
 
@@ -243,6 +272,7 @@ config_location_assets() {
         proxy_pass '"$alist_address"';
         proxy_set_header Accept-Encoding "";
         sub_filter '"'$direct_link'"' '"'$direct_link_sign'"';
+        sub_filter '"'$login_cmd'"' '"'$login_cmd_sign'"';
         sub_filter '"'$sign_str'"' '"''"';
         sub_filter '"'$sign_cond'"' '"'false'"';
         sub_filter_once off;
@@ -298,6 +328,64 @@ config_location_assets() {
 
 }
 
+config_location_getsignmd5() {
+    sign=$1
+    if [ -z "$sign" ]; then
+        NEW_CONFIG=''
+    else
+        NEW_CONFIG='
+    location /api/getsignmd5 {
+        '"$location_getsignmd5"'
+    }
+'
+    fi
+
+    # 临时文件
+    TMP_FILE=$(mktemp)
+
+    # 检查文件是否存在
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "Error: Nginx configuration file not found at $CONFIG_FILE"
+        exit 1
+    fi
+
+    # 处理文件
+    awk -v new_config="$NEW_CONFIG" '
+    BEGIN {
+        in_target_block = 0
+    }
+
+    # 匹配到目标server行时立即插入配置
+    /server \{/ {
+        print
+        # 立即插入新配置
+        printf("%s",new_config)
+        in_target_block = 1
+        next
+    }
+
+    # 在目标块中检查现有的location /assets
+    in_target_block && /location \/api\/getsignmd5/ {
+        # 跳过现有的if块（不打印）
+        while (getline > 0) {
+            if (/}[[:space:]]*$/) {
+                break
+            }
+        }
+	in_target_block = 0
+        next
+    }
+
+    # 其他情况直接打印
+    {
+        print
+    }
+    ' "$CONFIG_FILE" | sed '/^[[:space:]]*$/N; /^\n$/D' > "$TMP_FILE"
+
+    mv -f "$TMP_FILE" "$CONFIG_FILE"
+
+}
+
 config_location "$sign"
 config_uri_map "$sign"
 config_geo "$sign"
@@ -305,6 +393,10 @@ config_geo "$sign"
 #给网页直链加上签名
 config_geo_dollar "$sign"
 config_location_assets "$sign"
+
+#给nginx放开获取signmd5的api白名单
+config_location_getsignmd5 "$sign"
+
 if [ -f /run/nginx/nginx.pid ]; then
     nginx -s reload
 fi
